aaf0d90b9e96a00d0024baaa7c337910c9e56be6
leech <hyeong3244@gmail.com>
2025-04-25 00:17:49 +0900

    feat: 로그인 화면 기본 구조 생성

---
+++ front_end/src/views/LoginPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="login">
+      <!-- 로그인 폼 -->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Login',
+    data() {
+      return {};
+    },
+    methods: {},
+  };
+  </script>
+  
+  <style scoped>
+  /* 추후 스타일 추가 */
+  </style>
