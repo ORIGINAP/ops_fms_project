adbbdaab372ce546a9743a4558ad0a081e4d256f
leech <hyeong3244@gmail.com>
2025-04-25 12:46:56 +0900

    style: 레이아웃 및 제목 스타일 적용

--- front_end/src/views/LoginPage.vue
+++ front_end/src/views/LoginPage.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="login">
+    <h2 class="title">로그인</h2>
     <input v-model="username" placeholder="아이디" class="input" />
     <input v-model="password" type="password" placeholder="비밀번호" class="input" />
+    <button @click="login" class="btn">로그인</button>
   </div>
 </template>
-<button @click="login" class="btn">로그인</button>
   
 <script>
 export default {
@@ -31,6 +32,22 @@ export default {
 </script>
   
 <style scoped>
+.login-wrapper {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  width: 300px;
+  margin: 100px auto;
+}
+
+.title {
+  font-size: 24px;
+  font-weight: bold;
+  text-align: center;
+  color: #254081;
+  margin-bottom: 1rem;
+}
+
 .input {
   border: 1px solid #ccc;
   border-radius: 6px;
@@ -46,7 +63,9 @@ export default {
   cursor: pointer;
   margin-top: 10px;
 }
+
 .btn:hover {
   background-color: #1f3268;
 }
+
 </style>
